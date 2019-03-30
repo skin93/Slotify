0cbb890e3e6bef7d8bcd91d5e338e11f5ab312ba
skin93 <arro.ciesielski@gmail.com>
2019-03-30 22:51:18 +0100

    separing, validating, sanitizing, error messages

---
+++ includes/classes/Constants.php
@@ -0,0 +1,13 @@
+<?php
+    class Constants {
+
+        public static $passwordsDoNoMatch = "Your passwords don't match";
+        public static $passwordNotAlphanumeric = "Your password can only contain numbers and letters";
+        public static $passwordCharacters = "Your passowrd mus tbe between 5 and 30 characters";
+        public static $emailsDoNotMatch = "Your emails don't match";
+        public static $emailInvalid = "Email is invalid";
+        public static $lastNameCharacters = "Your last name must be between 5 and 25 characters";
+        public static $firstNameCharacters = "Your first name must be between 5 and 25 characters";
+        public static $usernameCharacters = "Your username must be between 5 and 25 characters";
+    }
+?>
